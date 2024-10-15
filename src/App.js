a752e3629a9330d78a2ea8776e05ec30fd8b5061
Mozhgan <mojganshah@gmail.com>
2024-10-15 16:02:37 +0330

    a mini project by REACT creating Navbar and styled

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="nav-background">
+      <div className="nav-container">
+        <header className="nav-header">
+          <h1>
+            <a href="#" className="nav-logo">
+              Sabzlearn
+            </a>
+          </h1>
+        </header>
+        <nav>
+          <ul className="nav-ul">
+            <li>
+              <a href="#" className="nav-link  active-link">
+                Home
+              </a>
+            </li>
+            <li>
+              <a href="#" className="nav-link">
+                News
+              </a>
+            </li>
+            <li>
+              <a href="#" className="nav-link">
+                Contact
+              </a>
+            </li>
+            <li>
+              <a href="#" className="nav-link">
+                About
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </div>
   );
 }
