3675e0a1627487e798c2ef75dca825a7b90ded1d
Mozhgan <mojganshah@gmail.com>
2024-10-16 22:09:46 +0330

    spread the props

--- src/App.js
+++ src/App.js
@@ -2,11 +2,17 @@ import React from "react";
 import Product from "./Product/Product";
 import "./App.css";
 function App() {
+  const allProducts = [
+    { id: 1, name: "Honor 8", image: "honor.png", price: 120 },
+    { id: 2, name: "iPhone 15 Pro", image: "iphone.png", price: 700 },
+    { id: 3, name: "Samsung Fold", image: "samsung.png", price: 540 },
+  ];
+  console.log({ ...allProducts[0] });
   return (
     <div className="container">
-      <Product name="Honor 8" image="honor.png" price={120} />
-      <Product name="iPhone 15 Pro" image="iphone.png" price={700} />
-      <Product name="Samsung Fold" image="samsung.png" price={540} />
+      <Product {...allProducts[0]} />
+      <Product {...allProducts[1]} />
+      <Product {...allProducts[2]} />
     </div>
   );
 }
