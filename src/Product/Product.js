012322a01f97d0edca1155fbab368920cc0bf249
Mozhgan <mojganshah@gmail.com>
2024-10-16 21:48:54 +0330

    obj destructuring

--- src/Product/Product.js
+++ src/Product/Product.js
@@ -1,13 +1,13 @@
 import "./Product.css";
 
-function Product(props) {
+function Product({ name, image, price }) {
   return (
     <div>
       <h2>iphone</h2>
       <div className="card">
-        <img src={props.image} alt="product" />
-        <h1>{props.name}</h1>
-        <p className="price">${props.price}</p>
+        <img src={image} alt="product" />
+        <h1>{name}</h1>
+        <p className="price">${price}</p>
         <p>
           <button>Add to Cart</button>
         </p>
